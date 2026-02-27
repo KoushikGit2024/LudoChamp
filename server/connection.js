00253971f3bec606600dd3120f1b77b5a9e0b90f
Koushik Kar <koushikkar712@gmail.com>
2026-02-27 15:49:29 +0530

    Changed folder struct

--- server/connection.js
+++ server/connection.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 async function connect() {
     const URL=(process.env.NODE_ENV === "production") ? process.env.MONGO_URL : process.env.MONGO_URL_DEV;
-    console.log(URL)
+    // console.log(URL)
     try {
         await mongoose.connect(URL);
         console.log("Connected to MongoDB");
